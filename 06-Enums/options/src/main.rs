4aa63d8ded0bbb9f1fe3640bbba9b38a69bba260
Brendan Prednis <brendan.prednis@protonmail.com>
2022-01-15 11:10:41 -0800

    finished enums.
    Signed-off-by: Brendan Prednis <brendan.prednis@protonmail.com>

--- 06-Enums/options/src/main.rs
+++ 06-Enums/options/src/main.rs
@@ -2,9 +2,54 @@
   OPTIONS AND ENUMS
 */
 fn main() {
-    let x: i8 = 5;
-    let y: Option<i8> = Some(5);
+    let cents = Coin::value_in_cents(Coin::Quarter(USState::California));
+    println!("{cents:?}");
 
-    let sum = x + y.unwrap_or(0);
-    println!("{sum}")
+    // Options
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("{six:?}, {none:?}");
+
+    if let Some(3) = five {
+        println!("three")
+    }
+}
+
+#[derive(Debug)]
+enum USState {
+    Oregon,
+    Vermont,
+    Arizona,
+    Washington,
+    California,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(USState),
+}
+
+impl Coin {
+    fn value_in_cents(coin: Coin) -> u32 {
+        match coin {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("State quarter from {state:?}");
+                25
+            }
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        Some(i) => Some(i + 1),
+        _ => None, // underscore matches anything that ISNT the exact thing we want.
+    }
 }
