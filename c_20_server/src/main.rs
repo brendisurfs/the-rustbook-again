5b2f886b5d6574563bcc49da8bd5d3dcb270cb1d
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-10-05 12:56:24 -0700

    single threaded server
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- c_20_server/src/main.rs
+++ c_20_server/src/main.rs
@@ -1,9 +1,11 @@
 use std::{
+    fs,
     io::{BufRead, BufReader, Read, Write},
     net::{TcpListener, TcpStream},
 };
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").expect("could not bind to tcp port");
+    println!("started server on 7878");
 
     // incoming() returns an iterator that gives us a sequence of streams
     for stream in listener.incoming() {
@@ -13,18 +15,17 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buf_reader = BufReader::new(&mut stream);
-    let mut buffer_vec = vec![0; 1024];
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    println!("request line: {}", request_line);
 
-    let http_response = buf_reader
-        .lines()
-        .map(|line| line.unwrap())
-        .take_while(|l| !l.is_empty())
-        .collect::<Vec<_>>();
-
-    println!("{}", String::from_utf8_lossy(&buffer_vec));
-
-    let repsonse = "HTTP/1.1 200 OK\r\n\r\n";
-
-    stream.write_all(repsonse.as_bytes()).unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let file_len = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {file_len}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
