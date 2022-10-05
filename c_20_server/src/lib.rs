2fd93bca4d2fb4954a16bd93897d124f1f7b202e
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-10-05 13:49:07 -0700

    updated worker struct
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- c_20_server/src/lib.rs
+++ c_20_server/src/lib.rs
@@ -1,4 +1,18 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+};
+impl Worker {
+    pub fn new(id: usize) -> Self {
+        let thread = thread::spawn(|| {});
+        Self {id, thread}
+    }
+}
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 pub enum PoolCreationError {
     SizeTooSmall,
 }
@@ -11,7 +25,15 @@ impl ThreadPool {
     ///
     pub fn build(size: usize) -> Result<Self, PoolCreationError> {
         if size > 0 {
-            Ok(Self)
+
+            let workers = Vec::with_capacity(size);
+            for id in 0..size {
+                workers.push(Worker::new(id));
+            }
+
+            Ok(Self {
+                workers,
+            })
         } else {
             Err(PoolCreationError::SizeTooSmall)
         }
